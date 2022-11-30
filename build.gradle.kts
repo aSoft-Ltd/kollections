550a8767f54f550961791e980a0f6d5af2092c05
andylamax <andylamax@programmer.net>
2022-11-30 04:48:46 +0300

    merging with dev

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,38 +11,20 @@
 }
 
 repositories {
-            publicRepos()
-            maven {
-                name = "piCortex"
-                url = uri("http://${picortex.versions.server.ip.get()}:1050/repository/internal/")
-                isAllowInsecureProtocol = true
-                credentials {
-                    username = "admin"
-                    password = "admin@123"
-                }
-            }
-            mavenLocal()
-		    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-        }
+	publicRepos()
+	mavenLocal()
+	maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+}
 
 val tmp = 0
 allprojects {
     beforeEvaluate {
         repositories {
             publicRepos()
-            maven {
-                name = "piCortex"
-                url = uri("http://${picortex.versions.server.ip.get()}:1050/repository/internal/")
-                isAllowInsecureProtocol = true
-                credentials {
-                    username = "admin"
-                    password = "admin@123"
-                }
-            }
             mavenLocal()
 		    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
         }
-        group = "com.picortex"
-        version = picortex.versions.picortex.get()
+        group = "tz.co.asoft"
+        version = "0.0.0"
     }
 }
