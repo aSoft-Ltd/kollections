511068c30911cbbd855f9a70ac3582485012ae06
andylamax <andylamax@programmer.net>
2023-01-22 13:41:28 +0300

    Upgrading to Gen2 presenters

--- interoperable/src/commonMain/kotlin/kollections/CollectionUtils.kt
+++ interoperable/src/commonMain/kotlin/kollections/CollectionUtils.kt
@@ -8,12 +8,12 @@ import kotlin.collections.Map as KMap
 
 inline fun <E> KCollection<E>.toIMutableList(): MutableList<E> = MutableListWrapper(toMutableList())
 
-inline fun <E> KCollection<E>.toIList(): List<E> = MutableListWrapper(toMutableList())
+inline fun <E> KCollection<E>.toIList(): List<E> = if (this is List) this else MutableListWrapper(toMutableList())
 
 inline fun <E> KCollection<E>.toIMutableSet(): MutableSet<E> = MutableSetWrapper(toMutableSet())
 
-inline fun <E> KCollection<E>.toISet(): Set<E> = MutableSetWrapper(toMutableSet())
+inline fun <E> KCollection<E>.toISet(): Set<E> = if (this is Set) this else MutableSetWrapper(toMutableSet())
 
-inline fun <K, V> KMap<K, V>.toIMap(): Map<K, V> = MutableMapWrapper(toMutableMap())
+inline fun <K, V> KMap<K, V>.toIMap(): Map<K, V> = if (this is Map) this else MutableMapWrapper(toMutableMap())
 
 inline fun <K, V> KMap<K, V>.toIMutableMap(): MutableMap<K, V> = MutableMapWrapper(toMutableMap())
