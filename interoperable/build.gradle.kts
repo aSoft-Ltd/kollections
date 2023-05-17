8d23c8086d0f84aa0e825fc791b63c57d39454a2
andylamax <andylamax@programmer.net>
2023-05-17 20:12:13 +0300

    Upadated kotlinx libraries

--- interoperable/build.gradle.kts
+++ interoperable/build.gradle.kts
@@ -11,10 +11,10 @@ kotlin {
     if (Targeting.JS) js(IR) { library() }
 //    if (Targeting.WASM) wasm { library() }
     val osxTargets = if (Targeting.OSX) osxTargets() else listOf()
-//    val ndkTargets = if (Targeting.NDK) ndkTargets() else listOf()
+    val ndkTargets = if (Targeting.NDK) ndkTargets() else listOf()
     val linuxTargets = if (Targeting.LINUX) linuxTargets() else listOf()
     val mingwTargets = if (Targeting.MINGW) mingwTargets() else listOf()
-    val nativeTargets = osxTargets + /*ndkTargets +*/ linuxTargets + mingwTargets
+    val nativeTargets = osxTargets + ndkTargets + linuxTargets + mingwTargets
 
     sourceSets {
         val commonMain by getting {
